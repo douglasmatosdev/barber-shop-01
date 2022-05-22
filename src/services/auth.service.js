ee1f69476bf34bce408124fe72c9b7b5331016a2
douglasmatosdev <douglasmatosdev@gmail.com>
2022-05-22 19:28:08 -0300

    add prod url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://barber-shop-01.herokuapp.com/api/auth/" // "http://localhost:8080/api/auth/";
 
 class AuthService {
   login(username, password) {
